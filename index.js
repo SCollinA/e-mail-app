c34a89737902220656f3345a3132b4120f583513
Collin Argo <collinargo@gmail.com>
2019-02-01 00:39:01 -0500

    add nodemailer

--- index.js
+++ index.js
@@ -1,32 +1,52 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+require('dotenv').config()
 const nodemailer = require('nodemailer')
 
 const app = express()
-const port = 3000
+const port = 1961
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-let transporter = nodemailer.createTransport({
-    sendmail: true,
-    path: '/usr/sbin/sendmail'
+// let transporter = nodemailer.createTransport({
+//     sendmail: true,
+//     newline: 'unix',
+//     path: '/usr/sbin/sendmail'
+// });
     // host: "smtp.gmail.email",
-        // port: 465, // 465 is ssl, 587 is tls
-        // secure: true, // true for 465, false for other ports
-        // auth: {
-        //   user: account.user, // generated ethereal user
-        //   pass: account.pass // generated ethereal password
-        // }
-})
+    //     port: 465, // 465 is ssl, 587 is tls
+    //     secure: true, // true for 465, false for other ports
+    //     auth: {
+    //       user: account.user, // generated ethereal user
+    //       pass: account.pass // generated ethereal password
+    //     } 
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    // service: 'gmail',
+    auth: {
+        type: 'OAUTH2',
+        user: process.env.E_MAIL, // generated ethereal user
+        serviceClient: process.env.SERVICE_CLIENT,
+        privateKey: process.env.PRIVATE_KEY,
+        pass: 'notasecret'
+    }
+})  
 
 app.post('/contactKelly', (req, res) => {
+    console.log('received request')
+    console.log(req.body)
     const { name, email, message, artwork } = req.body
     transporter.sendMail({
-        from: 'contact@example.com',
+        from: 'collinargo@gmail.com',
         to: 'collin.argo@gmail.com',
         subject: 'art-gallery contact',
-        text: `${name}${email}${message}${artwork.title}`
+        text: `${name}${email}${message}${artwork}`
     }, (err, info) => {
+        console.log(err)
         console.log(info.envelope)
         console.log(info.messageId)
         res.send(info)
