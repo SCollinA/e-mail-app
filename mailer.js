f29e3d10583f4948de53f4c7c62a17cc7a844180
Collin Argo <collinargo@gmail.com>
2019-01-31 20:49:56 -0500

    init commit

---
+++ mailer.js
@@ -0,0 +1,11 @@
+const nodemailer = require('nodemailer')
+
+let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: account.user, // generated ethereal user
+          pass: account.pass // generated ethereal password
+        }
+    })
